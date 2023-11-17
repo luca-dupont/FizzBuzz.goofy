0eb5ed116394834668e5c6b9e85643c6ac7dc087
Luca Dupont <67842853+luca-dupont@users.noreply.github.com>
2023-11-16 20:38:01 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+[print
+   (''.join(
+[{3:'Fizz'
+      ,5:    'Buzz',7:
+'Fuzz',                        11:'Bizz'}[
+e] for e in {3:'Fizz'
+,5:        'Buzz',7:      'Fuzz',
+11:'Bizz'}if not i%e]
+)               or i ) for i 
+                                                        in   range(1
+,int(input
+  ("How many numbers ? "
+))+1)]
